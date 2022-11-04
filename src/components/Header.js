34183e1f3d8485efee9948c91f40e6518037ff26
khanyisile <khanyisilegamafelicity@gmail.com>
2022-11-04 12:15:27 +0200

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+        <div className="main-info">
+            <h1>Welcometh To Mine Own Portfolio</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className='container'>
+                <a className="navbar-brand" href="#">My Portfolio</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr -auto">
+                        <li className="nav-item active">
+                        <span className="nav-link">Home <span className='sr-only'></span></span>
+                        </li>
+                        <li className="nav-item">
+                            <span className="nav-link">About Me</span>
+                        </li>
+                        <li className="nav-item">
+                            <span className="nav-link">Education</span>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Skills</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Why SovTech</a>
+                        </li>
+                    </ul> 
+                </div>
+            </div>
+        </nav>  
+    )
+}
+
+export default Navbar
