205fe8c1db0350b32fba3f2a6bc3a0a17d4ec04d
khanyisile <khanyisilegamafelicity@gmail.com>
2022-11-04 12:26:35 +0200

    Removed a tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className='container'>
-                <a className="navbar-brand" href="#">My Portfolio</a>
+                <span className="navbar-brand">My Portfolio</span>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -21,10 +21,10 @@ const Navbar = () => {
                             <span className="nav-link">Education</span>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Skills</a>
+                            <span className="nav-link">Skills</span>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Why SovTech</a>
+                            <span className="nav-link">Why SovTech</span>
                         </li>
                     </ul> 
                 </div>
